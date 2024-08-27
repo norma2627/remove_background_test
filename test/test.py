001d8980cb276e6a4f35c6fc708388994092617e
norma2627 <11norma11norma@gmail.com>
2024-08-27 13:36:37 +0900

    add some files

---
+++ test/test.py
@@ -0,0 +1,9 @@
+# main.py
+
+from remove_background import remove_background
+
+# Example usage
+input_path = "./img/maashi1.png"
+output_path = "./result/maashi1.png"
+
+remove_background(input_path, output_path)
