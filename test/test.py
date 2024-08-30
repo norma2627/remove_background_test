e2b35c73667cfe66833a71e68a43125162a0b233
norma2627 <11norma11norma@gmail.com>
2024-08-31 04:38:43 +0900

    fix readme.md

--- test/test.py
+++ test/test.py
@@ -3,7 +3,7 @@
 from remove_background import remove_background
 
 # Example usage
-input_path1 = "./img/maashi1.png"
-output_path1 = "./result/maashi.png"
+input_path1 = "./img/test.png"
+output_path1 = "./result/test.png"
 
 remove_background(input_path1, output_path1)
